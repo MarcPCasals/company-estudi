763948d2a641124fd4df1a565c31ffb9a84534e2
Marc Perez Casals <mperezc@educand.ad>
2026-07-12 13:56:27 +0200

    Configura la publicació a GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/company-estudi/' : '/',
   plugins: [react()],
   test: {
     environment: 'node',
     include: ['src/**/*.test.js'],
   },
 })
-
